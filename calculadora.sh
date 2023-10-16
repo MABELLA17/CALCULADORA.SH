34264e7d33ab92007bc3074d8c2e17fc075dbe3b
MABELLA17 <147401828+MABELLA17@users.noreply.github.com>
2023-10-16 07:51:37 -0300

    Add files via upload

--- calculadora.sh
+++ calculadora.sh
@@ -1,72 +1,72 @@
 #opções--------------------------------------------------------------------------------------
-echo "digite a operação que deseja realizar: "
+echo "digite a operação que deseja realizar: " 
 echo "1) SOMA"
 echo "2) SUBTRAÇÃO"
 echo "3) MULTIPLICAÇÃO"
 echo "4) DIVISÃO"
 echo "5) RAIZ QUADRADA"
 echo "6) POTÊNCIA"
-echo "operação: " $op
+echo "Opção desejada: " $op
 read 
 
 num=""
 num1=""
-num2=""
+num2="" 
 
 case $op in
 #soma----------------------------------------------------------------------------------------
-      1)    
-            read -p "Digite o primeiro número: " $num1
-            read -p "Digite o segundo número: " $num2
-                  resultado=$(echo "$num1+$num2" | bc)
-                  echo "Resultado final: $resultado"
-                  ;;    
+	1)
+		      echo "Digite o primeiro número: " $num1
+		      echo "Digite o segundo número: " $num2
+		resultado=$(echo "$num1+$num2" | bc)
+		echo "Soma final: $resultado"
+		;;
 
 #subtração-----------------------------------------------------------------------------------
-      2)
-            read -p "Digite o primeiro número: " $num1
-            read -p "Digite o segundo número: " $num2
-                  resultado=$(echo "$num1-$num2" | bc)
-                  echo "Resultado final: $resultado"
-                  ;;          
+	2)
+		      echo "Digite o primeiro número: " $num1
+		      echo "Digite o segundo número: " $num2
+		resultado=$(echo "$num1-$num2" | bc)
+		echo "Subtração final: $resultado"
+		;;
 
 #multiplicação-------------------------------------------------------------------------------
-      3) 
-            read -p "Digite o primeiro número: " $num1
-            read -p "Digite o segundo número: " $num2
-      resultado=$(echo "$num1*$num2" | bc)
-      echo "Resultado final: $resultado"
-      ;;
+	3)
+		      echo "Digite o primeiro número: " $num1
+		      echo "Digite o segundo número: " $num2
+		resultado=$(echo "$num1*$num2" | bc)
+		echo "Multiplicação final: $resultado"
+		;;
 
 #divisão-------------------------------------------------------------------------------------
-      4) 
-                  read -p "Digite o primeiro número: " $num1
-                  read -p "Digite o segundo número: " $num2
-            if [$num2 = 0]; then
-                  echo "ERRADO! Não é possível dividir por 0."
-            else
-            resultado=$(echo "scale=6; $num1/$num2" | bc)
-            echo "Resultado final: $resultado"
-            fi
-            ;;
+	4)
+		      echo "Digite o primeiro número: " $num1
+		      echo "Digite o segundo número: " $num2
+		if [$num2 = 0]; then
+		      echo "ERRADO! Não é possível dividir por 0."
+		else
+		resultado=$(echo "scale=6; $num1/$num2" | bc)
+		echo "Divisão final: $resultado"
+		fi
+		;;
 
 #raíz quadrada-------------------------------------------------------------------------------
-      5) 
-                  read -p "Digite um número: " $num
-            if [$num -lt 0]; then
-                  echo "ERRADO! Não é possível fazer raiz quadrada com número negativo."
-            else
-            resultado=$(echo "sqrt($num)" | bc)
-            echo "Resultado final: $resultado"
-            fi
-            ;;
+	5)
+			echo "Digite um número: " $num
+		if [$num = 0]; then
+		      echo "ERRADO! Não é possível fazer raiz quadrada com 0"
+		else
+			resultado=$(echo "sqrt($num)" | bc)
+		echo "Raiz quadrada final: $resultado"
+		fi
+		;;
 
 #potência------------------------------------------------------------------------------------
-      7) 
-                  read -p "Digite o primeiro número para ser a base: " $num1
-                  read -p "Digite o primeiro número para ser o expoente: " $num2
-            resultado=$(echo "$num1^$num2" | bc)
-            echo "Resultado final: $resultado"
-            ;;
+	6)
+		      echo "Digite um número para ser a base: " $num1
+		      echo "Digite um número para ser o expoente: " $num2
+		resultado=$(echo "$num1**$num2" | bc)
+		echo "Potência final: $resultado"
+		;;
+	esac
 
-esac
